55688c2efe560ec9c9d5ee05b1922a6ae854be0c
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-06-15 00:08:03 +0900

    counter which has 3 btns change text color before and after 0

---
+++ Counter/app.js
@@ -0,0 +1,28 @@
+//initial count
+let count=0;
+
+const value=document.querySelector('#value');
+
+const btns=document.querySelectorAll(".btn");
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        const styles=e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }else if(styles.contains('increase')){
+            count++;
+        }else if(styles.contains('reset')){
+            count=0;
+        }
+        
+        if(count<0){
+           value.style.color='red';
+        }else if(count>0){
+            value.style.color='blue';
+        }else{
+            value.style.color='black';
+        }
+
+        value.textContent=count;
+    })
+})
