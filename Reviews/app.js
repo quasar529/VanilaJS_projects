cf0e603b8cf1b2aa644a4b8900d21c5891049504
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-06-15 18:09:09 +0900

    nba players description it has next,prev,random btns.

---
+++ Reviews/app.js
@@ -0,0 +1,70 @@
+const reviews=[
+    {
+        id:1,
+        name:"Dwyane Wade",
+        job:"Shooting Guard",
+        img:"images/DWADE.png",
+        text:" DW is one of the greatest SG in the NBA history. He is Miami Heat francise superstar and he leads 3 time champion."
+    },
+    {
+        id:2,
+        name:"Allen Iverson",
+        job:"Shooting Guard",
+        img:"images/AI.png",
+        text:" AI is one of the best scorer in the NBA history. In 2001, 76ers, he leaded, was one and only team which beat Shaq's Lakers in the PlayOff."
+    },
+    {
+        id:3,
+        name:"Lebron James",
+        job:"Small Forward",
+        img:"images/LBJ.png",
+        text:" LBJ is the best SF of the all time. He wins 3 champion and he is incredible all-around player."
+    },
+    
+]
+//select item
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn=document.querySelector(".prev-btn");
+const nextBtn=document.querySelector(".next-btn");
+const randomBtn=document.querySelector(".random-btn");
+
+let currentItem=0;
+
+//load item
+window.addEventListener('DOMContentLoaded',function(){
+    showPerson();
+})
+
+function showPerson(){
+    const item=reviews[currentItem];
+    img.src=item.img;
+    author.textContent=item.name;
+    job.textContent=item.job;
+    info.textContent=item.text;
+}
+
+//show next
+nextBtn.addEventListener('click',function(){
+    currentItem++;
+    if(currentItem>reviews.length-1){
+        currentItem=0;
+    }
+    showPerson(currentItem);
+})
+//
+prevBtn.addEventListener('click',function(){
+    currentItem--;
+    if(currentItem<0){
+        currentItem=reviews.length-1;
+    }
+    showPerson();
+})
+//
+randomBtn.addEventListener('click',function(){
+    currentItem=Math.floor(Math.random()*reviews.length);
+    showPerson();
+})
